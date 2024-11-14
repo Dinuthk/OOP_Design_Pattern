822c140d9ed10d63bd0ede6dbae7f32cb9451eb3
Dinuthk <141445703+Dinuthk@users.noreply.github.com>
2024-11-15 00:57:34 +0530

    decorator

---
+++ Structural/Decorator/ex1/src/PepperoniDecorator.java
@@ -0,0 +1,16 @@
+// PepperoniDecorator.java
+public class PepperoniDecorator extends PizzaDecorator {
+    public PepperoniDecorator(Pizza decoratedPizza) {
+        super(decoratedPizza);
+    }
+
+    @Override
+    public String getDescription() {
+        return decoratedPizza.getDescription() + ", pepperoni";
+    }
+
+    @Override
+    public double cost() {
+        return decoratedPizza.cost() + 2.0; // Cost of pepperoni topping
+    }
+}
