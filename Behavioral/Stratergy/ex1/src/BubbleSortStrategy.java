3e733173ad1f72656166f13f650ad864d02b6763
Dinuthk <141445703+Dinuthk@users.noreply.github.com>
2024-11-16 13:16:18 +0530

    stratergy

---
+++ Behavioral/Stratergy/ex1/src/BubbleSortStrategy.java
@@ -0,0 +1,7 @@
+public class BubbleSortStrategy implements SortingStrategy {
+    @Override
+    public void sort(int[] array) {
+        // Implement Bubble Sort algorithm
+        System.out.println("Sorting using Bubble Sort");
+    }
+}
